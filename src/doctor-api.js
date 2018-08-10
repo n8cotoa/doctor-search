2a33617d3ccb3ab79683858e9ef97b3266a8f8b7
Nate Cottle <cottle.nate@gmail.com>
2018-08-10 10:16:10 -0700

    Add show and hide to switch between the two different search forms. Fix how phone numbers are displayed.

--- src/doctor-api.js
+++ src/doctor-api.js
@@ -1,9 +1,25 @@
 export class DoctorService {
+  
   getBySymptom(userSymptom) {
-
     return new Promise(function(resolve, reject) {
       const request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${userSymptom}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open('GET', url, true);
+      request.send();
+    });
+  }
+
+  getByName(doctorName) {
+    return new Promise(function(resolve, reject) {
+      const request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -5,15 +5,23 @@ import './styles.css';
 import { DoctorService } from './doctor-api.js';
 
 $(document).ready(function() {
-  $('#symptomSearch').click(function(e) {
 
+  $('#symptomSearch').click(function(e) {
+    $('#symptom-form').show();
+    $('#doctor-form').hide();
+    $('#results').text("")
+  });
+
+  $('#doctorSearch').click(function(e) {
+    $('#symptom-form').hide();
+    $('#doctor-form').show();
+    $('#results').text("")
   });
 
   $('#symptom-form').submit(function(e){
     e.preventDefault();
+    $('#results').text("")
     let userSymptom = $("#user-symptom").val();
-    // let doctorName = $('#doctor-name').val();
-
     let doctorSearch = new DoctorService();
     let promiseOfHealth = doctorSearch.getBySymptom(userSymptom);
 
@@ -28,7 +36,34 @@ $(document).ready(function() {
             `<div class="card" style="width: 20rem;">
               <div class="card-body">
                 <h5 class="card-title">${practice.name}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${practice.phones[0].number}, ${practice.phones[0].type}</h6>
+                <h6 class="card-subtitle mb-2 text-muted">Phone: ${(practice.phones[0].type == 'fax' ? practice.phones[1].number : practice.phones[0].number)}</h6>
+                <p class="card-text">${address}</p>
+                <p>${(practice.accepts_new_patients ? "Accepting new patients" : "Not accepting new patients")}</p>
+              </div>
+            </div>`
+          );
+        });
+      });
+    });
+  });
+  $('#doctor-form').submit(function(e){
+    e.preventDefault();
+    let doctorName = $('#doctor-name').val();
+    let doctorSearch = new DoctorService();
+    let promiseOfHealth = doctorSearch.getByName(doctorName);
+
+    promiseOfHealth.then(function(response) {
+      let body = JSON.parse(response);
+      console.log(body);
+      body.data.forEach(function(data) {
+        data.practices.forEach(function(practice) {
+          let location = practice.visit_address;
+          let address = `${location.street}, ${location.city}, ${location.state} ${location.zip}`;
+          $("#results").append(
+            `<div class="card" style="width: 20rem;">
+              <div class="card-body">
+                <h5 class="card-title">${practice.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Phone: ${(practice.phones[0].type == 'fax' ? practice.phones[1].number : practice.phones[0].number)}</h6>
                 <p class="card-text">${address}</p>
                 <p>${(practice.accepts_new_patients ? "Accepting new patients" : "Not accepting new patients")}</p>
               </div>
