a8b04ee869cc735a620c92ce0e01ea33bc75db7e
Nate Cottle <cottle.nate@gmail.com>
2018-08-10 08:31:45 -0700

    Add class and method for calling doctor api.

---
+++ src/doctor-api.js
@@ -0,0 +1,20 @@
+export class DoctorService {
+  getDoctors(attributes) {
+    return new Promise(resolve, reject) {
+      let doctorName = attributes.name, "";
+      let userSymptom = attributes.symptom, "";
+
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${userSymptom}&name=${doctorName}&location=or-portland&skip=0&limit=10&user_key=${process.env.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open('GET', url, true);
+      request.send();
+    }
+  }
+}
