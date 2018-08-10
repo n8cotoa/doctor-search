9cdd30d41aa625c30fbbd5d1c14ab38aec668966
Nate Cottle <cottle.nate@gmail.com>
2018-08-10 10:49:06 -0700

    Add selection of conditions based on conditions listed in the database, add error message returns, add no doctors found if query returns nothing.

--- src/doctor-api.js
+++ src/doctor-api.js
@@ -1,5 +1,5 @@
 export class DoctorService {
-  
+
   getBySymptom(userSymptom) {
     return new Promise(function(resolve, reject) {
       const request = new XMLHttpRequest();
@@ -10,7 +10,7 @@ export class DoctorService {
         } else {
           reject(Error(request.statusText));
         }
-      }
+      };
       request.open('GET', url, true);
       request.send();
     });
@@ -26,7 +26,22 @@ export class DoctorService {
         } else {
           reject(Error(request.statusText));
         }
-      }
+      };
+      request.open('GET', url, true);
+      request.send();
+    });
+  }
+  getConditions() {
+    return new Promise(function(resolve, reject) {
+      const request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
       request.open('GET', url, true);
       request.send();
     });
--- src/main.js
+++ src/main.js
@@ -5,48 +5,69 @@ import './styles.css';
 import { DoctorService } from './doctor-api.js';
 
 $(document).ready(function() {
+  let conditionSearch = new DoctorService();
+  let conditions = conditionSearch.getConditions();
 
-  $('#symptomSearch').click(function(e) {
-    $('#symptom-form').show();
-    $('#doctor-form').hide();
-    $('#results').text("")
+  conditions.then(function(response){
+    let body = JSON.parse(response);
+    body.data.forEach(function(condition){
+      $("#conditions").append(`<option>${condition.name}</option>`);
+    });
+  },function(error) {
+    $('.showError').text(`There was an error: ${error.message}`);
   });
 
-  $('#doctorSearch').click(function(e) {
+  $('#symptomSearch').click(function() {
+    $('#symptom-form').show();
+    $('#doctor-form').hide();
+    $('#results').text("");
+    $('.showError').text('');
+  });
+
+  $('#doctorSearch').click(function() {
     $('#symptom-form').hide();
     $('#doctor-form').show();
-    $('#results').text("")
+    $('#results').text("");
+    $('.showError').text('');
   });
 
   $('#symptom-form').submit(function(e){
     e.preventDefault();
-    $('#results').text("")
-    let userSymptom = $("#user-symptom").val();
+    $('#results').text("");
+    $('.showError').text('');
+    let userSymptom = $("#conditions").val();
     let doctorSearch = new DoctorService();
     let promiseOfHealth = doctorSearch.getBySymptom(userSymptom);
 
     promiseOfHealth.then(function(response) {
       let body = JSON.parse(response);
-      console.log(body);
-      body.data.forEach(function(data) {
-        data.practices.forEach(function(practice) {
-          let location = practice.visit_address;
-          let address = `${location.street}, ${location.city}, ${location.state} ${location.zip}`;
-          $("#results").append(
-            `<div class="card" style="width: 20rem;">
-              <div class="card-body">
-                <h5 class="card-title">${practice.name}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Phone: ${(practice.phones[0].type == 'fax' ? practice.phones[1].number : practice.phones[0].number)}</h6>
-                <p class="card-text">${address}</p>
-                <p>${(practice.accepts_new_patients ? "Accepting new patients" : "Not accepting new patients")}</p>
-              </div>
-            </div>`
-          );
+      if (body.data !== []) {
+        body.data.forEach(function(data) {
+          data.practices.forEach(function(practice) {
+            let location = practice.visit_address;
+            let address = `${location.street}, ${location.city}, ${location.state} ${location.zip}`;
+            $("#results").append(
+              `<div class="card" style="width: 20rem;">
+                <div class="card-body">
+                  <h5 class="card-title">${practice.name}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Phone: ${(practice.phones[0].type == 'fax' ? practice.phones[1].number : practice.phones[0].number)}</h6>
+                  <p class="card-text">${address}</p>
+                  <p>${(practice.accepts_new_patients ? "Accepting new patients" : "Not accepting new patients")}</p>
+                </div>
+              </div>`
+            );
+          });
         });
-      });
+      } else {
+        $('.showError').text('No doctors meet this search query');
+      }
+    },function(error) {
+      $('.showError').text(`There was an error: ${error.message}`);
     });
   });
   $('#doctor-form').submit(function(e){
+    $('#results').text("");
+    $('.showError').text('');
     e.preventDefault();
     let doctorName = $('#doctor-name').val();
     let doctorSearch = new DoctorService();
@@ -54,23 +75,28 @@ $(document).ready(function() {
 
     promiseOfHealth.then(function(response) {
       let body = JSON.parse(response);
-      console.log(body);
-      body.data.forEach(function(data) {
-        data.practices.forEach(function(practice) {
-          let location = practice.visit_address;
-          let address = `${location.street}, ${location.city}, ${location.state} ${location.zip}`;
-          $("#results").append(
-            `<div class="card" style="width: 20rem;">
-              <div class="card-body">
-                <h5 class="card-title">${practice.name}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Phone: ${(practice.phones[0].type == 'fax' ? practice.phones[1].number : practice.phones[0].number)}</h6>
-                <p class="card-text">${address}</p>
-                <p>${(practice.accepts_new_patients ? "Accepting new patients" : "Not accepting new patients")}</p>
-              </div>
-            </div>`
-          );
+      if (body.meta.total !== 0) {
+        body.data.forEach(function(data) {
+          data.practices.forEach(function(practice) {
+            let location = practice.visit_address;
+            let address = `${location.street}, ${location.city}, ${location.state} ${location.zip}`;
+            $("#results").append(
+              `<div class="card" style="width: 20rem;">
+                <div class="card-body">
+                  <h5 class="card-title">${practice.name}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Phone: ${(practice.phones[0].type == 'fax' ? practice.phones[1].number : practice.phones[0].number)}</h6>
+                  <p class="card-text">${address}</p>
+                  <p>${(practice.accepts_new_patients ? "Accepting new patients" : "Not accepting new patients")}</p>
+                </div>
+              </div>`
+            );
+          });
         });
-      });
+      } else {
+        $('.showError').text('No doctors meet this search query');
+      }
+    },function(error) {
+      $('.showError').text(`There was an error: ${error.message}`);
     });
   });
 });
